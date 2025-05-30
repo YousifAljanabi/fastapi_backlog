97205071ac65d50e2763ed524e7bcb1ef2692b25
YousifA <yosif.ph2003@gmail.com>
2025-05-30 14:43:03 +0300

    Initial project and database connection

---
+++ app/db/Session.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+from typing import AsyncGenerator
+import os
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+load_dotenv()
+
+db_connection_string = f"postgresql://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+async_db_connection_string = f"postgresql+asyncpg://{os.getenv("DB_USER")}:{os.getenv("DB_PASS")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}"
+
+engine = create_async_engine(async_db_connection_string, echo=True, future=True)
+session_maker = async_sessionmaker(engine, expire_on_commit=False)
+
+Base = declarative_base()
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with session_maker() as session:
+        yield session
